2501aa3923573903754606f35400b51fdebb8d10
huangduyu <huangduyu@douban.com>
2016-11-10 15:25:51 +0800

    Initial Commit

---
+++ DyQQSDK/DyQQSDK.h
@@ -0,0 +1,19 @@
+//
+//  DyQQSDK.h
+//  DyQQSDK
+//
+//  Created by bigyelow on 10/11/2016.
+//  Copyright © 2016 huangduyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DyQQSDK.
+FOUNDATION_EXPORT double DyQQSDKVersionNumber;
+
+//! Project version string for DyQQSDK.
+FOUNDATION_EXPORT const unsigned char DyQQSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DyQQSDK/PublicHeader.h>
+
+
